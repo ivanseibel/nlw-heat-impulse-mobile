1201f1c4204c4334eeae235432627162b973c59d
Ivan L. Seibel <me@ivanseibel.dev>
2021-11-02 01:42:29 -0300

    Implement socket to get new messages

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,17 +1,46 @@
 import React, { useEffect, useState } from 'react';
 import { ScrollView } from 'react-native';
+import { io } from 'socket.io-client';
+
 import { api } from '../../services/api';
 import { Message, MessageProps } from '../Message';
 
 import { styles } from './styles';
 
+const messagesQueue: MessageProps[] = [];
+
+const socket = io(String(api.defaults.baseURL));
+
+socket.on("new_message", (newMessage: MessageProps) => {
+  messagesQueue.push(newMessage);
+});
+
 export const MessageList: React.FC = () => {
   const [currentMessages, setCurrentMessages] = useState<MessageProps[]>([]);
 
   useEffect(() => {
-    api.get<MessageProps[]>('/messages/last3').then((response) => {
+    const timer = setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setCurrentMessages(state => [
+          messagesQueue[0],
+          state[0],
+          state[1],
+        ].filter(Boolean));
+        messagesQueue.shift();
+      }
+    }, 3000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  useEffect(() => {
+    setCurrentMessages([]);
+    const fetchMessages = async () => {
+      const response = await api.get<MessageProps[]>('/messages/last3');
       setCurrentMessages(response.data);
-    });
+    }
+
+    fetchMessages();
   }, []);
 
   return (
@@ -20,9 +49,12 @@ export const MessageList: React.FC = () => {
       contentContainerStyle={styles.contentContainer}
       keyboardShouldPersistTaps="never"
     >
-      {currentMessages.map(item => (
-        <Message key={item.id} data={item} />
-      ))}
+      {currentMessages.map(item => {
+        // console.log(item);
+        return (
+          <Message key={item.id} data={item} />
+        );
+      })}
 
     </ScrollView>
   );
--- src/components/SendMessageForm/index.tsx
+++ src/components/SendMessageForm/index.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import { Text, TextInput, View } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { Alert, Keyboard, Text, TextInput, View } from 'react-native';
+import { api } from '../../services/api';
 import { COLORS } from '../../theme';
 import Button from '../Button';
 
@@ -8,6 +9,25 @@ import { styles } from './styles';
 export const SendMessageForm: React.FC = () => {
   const [message, setMessage] = useState('');
   const [sendingMessage, setSendingMessage] = useState(false);
+
+  const handleSendMessage = useCallback(async () => {
+    if (!message.trim()) {
+      Alert.alert('Type a message to send.')
+      return;
+    }
+
+    Keyboard.dismiss();
+
+    try {
+      setSendingMessage(true);
+      await api.post('messages', { text: message.trim() });
+      setMessage('');
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSendingMessage(false);
+    }
+  }, [message, setSendingMessage, api]);
 
   return (
     <View style={styles.container}>
@@ -27,6 +47,8 @@ export const SendMessageForm: React.FC = () => {
         title="SEND MESSAGE"
         backgroundColor={COLORS.PINK}
         color={COLORS.WHITE}
+        onPress={handleSendMessage}
+        isLoading={sendingMessage}
       />
     </View>
   );
